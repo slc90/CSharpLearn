b292212615cea9bb76b70398f3411f3bf4ff7878
slc90 <506091625@qq.com>
2024-05-27 20:18:56 +0800

    增加Skia工程

---
+++ SkiaLearn/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using SkiaSharp.Views.Desktop;
+using SkiaSharp;
+using System.Windows;
+
+namespace SkiaLearn;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+    {
+        // 获取绘图表面和画布
+        var surface = e.Surface;
+        var canvas = surface.Canvas;
+        // 清除画布
+        canvas.Clear(SKColors.White);
+        // 创建绘图对象
+        using var paint = new SKPaint();
+        paint.Color = SKColors.Red;
+        paint.IsAntialias = true;
+        // 绘制一个圆
+        canvas.DrawCircle(e.Info.Width / 2, e.Info.Height / 2, 100, paint);
+    }
+}
---
+++ SkiaLearn/Readme.md
@@ -0,0 +1,2 @@
+# 在WPF中使用Skia绘制图形
+1. 建立工程，安装SkiaSharp和SkiaSharp.Views.WPF的nuget包
